770c3da6dd88e9948911493c4be414eb33b88c3e
Mike Podonyi <mikepodonyi@gmail.com>
2013-04-18 17:20:08 +0100

    code drop

---
+++ src/MPConditions.Test/UniversalNumberComparerTest.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MPConditions.Numeric;
+using FluentAssertions;
+
+namespace MPConditions.Test
+{
+    [TestClass]
+    public class UniversalNumberComparerTest
+    {
+        private const int LeftLessThanRight = -1;
+        private const int LeftEqualsRight = 0;
+        private const int LeftGreaterThanRight = 1;
+
+        private const byte      Byte    =1;
+        private const sbyte     SByte   =1;
+        private const short     Int16   =1;
+        private const ushort    UInt16  =1;
+        private const int       Int32   =1;
+        private const uint      UInt32  =1;
+        private const long      Int64   =1;
+        private const ulong     UInt64  =1;
+        private const decimal   Decimal =1.0M;
+        private const float     Single  =1.0F;
+        private const double    Double  =1.0;
+
+        private const double DoubleNaN = double.NaN;
+        private const double DoubleInfinitePos = double.PositiveInfinity;
+        private const double DoubleInfiniteNeg = double.NegativeInfinity;
+
+        private const object Null= null;
+
+        [TestMethod]
+        public void ByteCompare()
+        {
+            var unc = new UniversalNumberComparer();
+
+            //weird
+            unc.Compare(Byte, Null).Should().Be(LeftGreaterThanRight);
+            unc.Compare(Byte, DoubleNaN).Should().Be(LeftGreaterThanRight);
+            unc.Compare(Byte, DoubleInfinitePos).Should().Be(LeftLessThanRight);
+            unc.Compare(Byte, DoubleInfiniteNeg).Should().Be(LeftGreaterThanRight);
+
+            unc.Compare(Null, Byte).Should().Be(LeftLessThanRight);
+            unc.Compare(DoubleNaN, Byte).Should().Be(LeftLessThanRight);
+            unc.Compare(DoubleInfinitePos, Byte).Should().Be(LeftGreaterThanRight);
+            unc.Compare(DoubleInfiniteNeg, Byte).Should().Be(LeftLessThanRight);
+
+            //same
+
+            unc.Compare(Byte, Byte).Should().Be(LeftEqualsRight);
+            unc.Compare(Byte, SByte).Should().Be(LeftEqualsRight);
+            unc.Compare(Byte, Int16).Should().Be(LeftEqualsRight);
+            unc.Compare(Byte, UInt16).Should().Be(LeftEqualsRight);
+            unc.Compare(Byte, Int32).Should().Be(LeftEqualsRight);
+            unc.Compare(Byte, UInt32).Should().Be(LeftEqualsRight);
+            unc.Compare(Byte, Int64).Should().Be(LeftEqualsRight);
+            unc.Compare(Byte, UInt64).Should().Be(LeftEqualsRight);
+            unc.Compare(Byte, Decimal).Should().Be(LeftEqualsRight);
+            //unc.Compare(Byte, Single).Should().Be(LeftEqualsRight);
+            //unc.Compare(Byte, Double).Should().Be(LeftEqualsRight);
+
+            uint? y = 1;
+
+            unc.Compare(Byte, y).Should().Be(LeftEqualsRight);
+
+        }
+
+        [TestMethod]
+        public void ByteCompare2()
+        {
+            double d;
+
+            long l=long.MaxValue;
+            d = (double)l;
+            long l2=(long)d;
+            l.CompareTo(l2).Should().Be(0);
+
+            ulong ul = ulong.MaxValue;
+            d = (double)ul;
+            ulong ul2 = (ulong)d;
+            ul.CompareTo(ul2).Should().Be(0);
+
+            //float f=float.MaxValue;
+            //d=Convert.ToDecimal(f);
+            //float f2=(float)d;
+            //f.CompareTo(f2).Should().Be(0);
+
+            //double de= double.MaxValue;
+            //d=(decimal)de;
+            //double de2 = (double)d;
+            //de.CompareTo(de2).Should().Be(0);
+
+        }
+
+
+        [TestMethod]
+        public void ByteCompare3()
+        {
+            double? i=null;
+            double i2 = double.NaN;
+
+            object o=i;
+            ((IComparable<double>)o).CompareTo(i2);
+
+        }
+    }
+}
--- src/MPConditions/Numeric/UniversalNumberComparer.cs
+++ src/MPConditions/Numeric/UniversalNumberComparer.cs
@@ -5,36 +5,225 @@ using System.Text;
 
 namespace MPConditions.Numeric
 {
-    internal class UniversalNumberComparer : IComparer
+    public class UniversalNumberComparer : IComparer
     {
         public int Compare(object x, object y)
         {
-            if(!IsNumber(x) )
-                throw new ArgumentException("The argument is not a numeric type or null.", "x");
+            if(!IsNumber(x))
+                throw new ArgumentException("The argument is not a numeric type.", "x");
 
             if(!IsNumber(y))
-                throw new ArgumentException("The argument is not a numeric type or null.", "y");
+                throw new ArgumentException("The argument is not a numeric type.", "y");
 
-            return x == null
-               ? (y == null ? 0 : -1)
-               : (y == null ? 1 : Decimal.Compare(Convert.ToDecimal(x), Convert.ToDecimal(y)));
+            if(IsNull(x))
+            {
+                if(IsNull(y))
+                    return 0;
+
+                if(IsNaN(y))
+                    throw new Exception("Can't compare with isNan");
+
+                //Any other value is bigger
+                return -1;
+            }
+
+            if(IsNull(y))
+            {
+                //if(IsNull(x))
+                //    return 0;
+
+                if(IsNaN(x))
+                    return 1;
+
+                //Any other value is bigger
+                return 1;
+            }
+
+            if(IsNegativeInfinite(x))
+            {
+                if(IsNegativeInfinite(y))
+                    return 0;
+
+                if(IsNaN(y))
+                    return 1;
+
+                return -1;
+            }
+
+            if(IsNegativeInfinite(y))
+            {
+                if(IsNaN(x))
+                    return -1;
+
+                return 1;
+            }
+
+            if(IsPositivInfinite(x))
+            {
+                if(IsPositivInfinite(y))
+                    return 0;
+
+                if(IsNaN(y))
+                    return 1;
+
+                return 1;
+            }
+
+            if(IsPositivInfinite(y))
+            {
+                if(IsNaN(x))
+                    return -1;
+
+                return -1;
+            }
+
+            if(IsNaN(x))
+            {
+                if(IsNaN(y))
+                    return 0;
+
+                return -1;
+            }
+
+            if(IsNaN(y))
+            {
+                return 1;
+            }
+
+            //if you are here then both values are valid numbers
+            var convertionTypeX = GetConvertionType(x);
+            var convertionTypeY = GetConvertionType(y);
+
+            if(convertionTypeX == convertionTypeY)
+            {
+                return ((IComparable)Convert.ChangeType(x, convertionTypeX, null)).CompareTo(Convert.ChangeType(y, convertionTypeY, null));
+            }
+
+            throw new NotImplementedException();
+
         }
 
-        private static readonly TypeCode[] _TypeCode ={TypeCode.Byte,
-                                                TypeCode.Decimal,
-                                                TypeCode.Double,
-                                                TypeCode.Int16,
-                                                TypeCode.Int32,
-                                                TypeCode.Int64,
-                                                TypeCode.SByte,
-                                                TypeCode.Single,
-                                                TypeCode.UInt16,
-                                                TypeCode.UInt32,
-                                                TypeCode.UInt64};
+        private int CompareDoubleWithDecimal(double dob, decimal dec)
+        {
+            return 0;
+        }
+
+        private static Type GetConvertionType(object value)
+        {
+            switch(Type.GetTypeCode(value.GetType()))
+            {
+                case TypeCode.Byte:
+                case TypeCode.SByte:
+                case TypeCode.Int16:
+                case TypeCode.UInt16:
+                case TypeCode.Int32:
+                case TypeCode.UInt32:
+                case TypeCode.Int64:
+                case TypeCode.UInt64:
+                case TypeCode.Decimal:
+                    return typeof(decimal);
+                case TypeCode.Single:
+                case TypeCode.Double:
+                    return typeof(double);
+            }
+
+            return null;
+        }
+
+
+        //private static Type GetConvertionType(object x, object y)
+        //{ 
+        //    switch()
+
+        //}
+
+        //private bool IsNumber(object value)
+        //{
+        //    return (value == null) || (Array.IndexOf(_TypeCode, Type.GetTypeCode(value.GetType())) != -1);
+        //}
+
+        private bool IsNumberType(Type valuetype)
+        {
+            switch(Type.GetTypeCode(valuetype))
+            {
+                case TypeCode.Byte:
+                case TypeCode.Decimal:
+                case TypeCode.Double:
+                case TypeCode.Int16:
+                case TypeCode.Int32:
+                case TypeCode.Int64:
+                case TypeCode.SByte:
+                case TypeCode.Single:
+                case TypeCode.UInt16:
+                case TypeCode.UInt32:
+                case TypeCode.UInt64:
+                    return true;
+            }
+
+            return false;
+        }
 
         private bool IsNumber(object value)
         {
-            return (value == null) || (Array.IndexOf(_TypeCode, Type.GetTypeCode(value.GetType())) != -1);
+            if(value == null)
+                return true;
+
+            //Type underlyingtype = Nullable.GetUnderlyingType(value.GetType());
+
+            return //(underlyingtype != null)
+                //? IsNumberType(underlyingtype)
+            //    : 
+                IsNumberType(value.GetType());
+        }
+
+        private bool IsType(object value, TypeCode typeCode)
+        {
+            return typeCode == Type.GetTypeCode(value.GetType());
+        }
+
+        private bool IsNull(object value)
+        {
+            return (value == null);
+        }
+
+        private bool IsNegativeInfinite(object value)
+        {
+            switch(Type.GetTypeCode(value.GetType()))
+            {
+                case TypeCode.Single:
+                    return float.IsNegativeInfinity((float)value);
+                case TypeCode.Double:
+                    return double.IsNegativeInfinity((double)value);
+            }
+
+            return false;
+        }
+
+        private bool IsPositivInfinite(object value)
+        {
+            switch(Type.GetTypeCode(value.GetType()))
+            {
+                case TypeCode.Single:
+                    return float.IsPositiveInfinity((float)value);
+                case TypeCode.Double:
+                    return double.IsPositiveInfinity((double)value);
+            }
+
+            return false;
+        }
+
+
+        private bool IsNaN(object value)
+        {
+            switch(Type.GetTypeCode(value.GetType()))
+            {
+                case TypeCode.Single:
+                    return float.IsNaN((float)value);
+                case TypeCode.Double:
+                    return double.IsNaN((double)value);
+            }
+
+            return false;
         }
     }
 }
